e776d75b3b2b7ed3c3597e9b39dbc0bdbdc2c87e
Peter Roth <peter@roth.su>
2022-05-05 19:17:21 +0200

    Fix typo

--- bb.py
+++ bb.py
@@ -24,7 +24,7 @@ username = parser.get("config", "username")
 password = parser.get("config", "password")
 subreddit = parser.get("config", "subreddit")
 reddit = praw.Reddit(user_agent="GCP:Blog-bot:v1 (by u/ItsMeRPeter)", client_id=client_id,
-                     client_secret=client_secret, userme=username, password=password)
+                     client_secret=client_secret, username=username, password=password)
 
 while True:
     time.sleep(600)
